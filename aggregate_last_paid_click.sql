8b57b3d04128a80bb80741045686df2b758de6de
Chupshev Vlad <154665035+Vladyan0011@users.noreply.github.com>
2024-05-04 22:03:18 +0400

    Update aggregate_last_paid_click.sql

--- aggregate_last_paid_click.sql
+++ aggregate_last_paid_click.sql
@@ -6,7 +6,7 @@ WITH union_ads AS (
         utm_medium,
         SUM(daily_spent) AS spent
     FROM vk_ads
-    GROUP BY 1,2,3,4
+    GROUP BY 1, 2, 3, 4
     UNION
     SELECT
         DATE(campaign_date) AS campaign_date,
@@ -14,18 +14,20 @@ WITH union_ads AS (
         utm_campaign,
         utm_medium,
         SUM(daily_spent) AS spent
-    FROM ya_ads 
-    GROUP BY 1,2,3,4
+    FROM ya_ads
+    GROUP BY 1, 2, 3, 4
     ORDER BY 1
 ),
+
 tab AS (
     SELECT
         visitor_id,
         MAX(visit_date) AS last_visit
-    FROM sessions s
-    WHERE medium <> 'organic'
+    FROM sessions
+    WHERE medium != 'organic'
     GROUP BY visitor_id
 ),
+
 last_paid_attribution AS
 (
     SELECT
@@ -41,9 +43,10 @@ last_paid_attribution AS
         l.status_id
     FROM tab AS t
     INNER JOIN sessions s ON t.visitor_id = s.visitor_id AND t.last_visit = s.visit_date
-    LEFT JOIN leads AS l ON s.visitor_id =l.visitor_id AND t.last_visit <= l.created_at 
+    LEFT JOIN leads AS l ON s.visitor_id =l.visitor_id AND t.last_visit <= l.created_at
     ORDER BY l.amount DESC NULLS LAST, visit_date, utm_source, utm_medium, utm_campaign
 ),
+
 aggregate_last_paid AS (
     SELECT
         DATE(lpa.visit_date) AS visit_date,
@@ -52,14 +55,15 @@ aggregate_last_paid AS (
         lpa.utm_campaign,
         COUNT(DISTINCT lpa.visitor_id) AS visitors_count,
         COUNT(lpa.lead_id) AS leads_count,
-        COUNT(lpa.amount) 
+        COUNT(lpa.amount)
             FILTER (WHERE lpa.closing_reason = 'Успешно реализованно' OR lpa.status_id = 142)
             AS purchases_count,
         SUM(amount) AS revenue
     FROM last_paid_attribution lpa
     GROUP BY 1, 2, 3, 4
 )
-SELECT 
+
+SELECT
     alp.visit_date,
     alp.visitors_count,
     alp.utm_source,
@@ -70,9 +74,9 @@ SELECT
     alp.purchases_count,
     alp.revenue
 FROM aggregate_last_paid alp
-LEFT JOIN union_ads ua 
+LEFT JOIN union_ads ua
     ON alp.utm_source = ua.utm_source
     AND alp.utm_campaign = ua.utm_campaign
-    AND alp.utm_medium = ua.utm_medium 
+    AND alp.utm_medium = ua.utm_medium
     AND DATE(alp.visit_date) = ua.campaign_date
 ORDER BY revenue DESC NULLS LAST, visit_date, visitors_count DESC, utm_source, utm_medium, utm_campaign

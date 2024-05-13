2f9c93c377be203b017cabda2cccb3369c5aa560
Chupshev Vlad <154665035+Vladyan0011@users.noreply.github.com>
2024-05-13 12:31:02 +0400

    Update dashboard.sql

--- dashboard.sql
+++ dashboard.sql
@@ -3,34 +3,44 @@ WITH tab AS (
     SELECT
         visitor_id,
         MAX(visit_date) AS last_visit
-    FROM sessions s
-    WHERE medium <> 'organic'
+    FROM sessions
+    WHERE medium != 'organic'
     GROUP BY visitor_id
 ),
+
 diff_tab AS (
-SELECT
-    s.visitor_id,
-    s.visit_date,
-    s.source AS utm_source,
-    s.medium AS utm_medium,
-    s.campaign AS utm_campaign,
-    l.lead_id,
-    l.created_at,
-    l.amount,
-    l.closing_reason,
-    l.status_id,
-    l.created_at - s.visit_date AS diff
-FROM tab AS t
-INNER JOIN sessions s ON t.visitor_id = s.visitor_id AND t.last_visit = s.visit_date
-LEFT JOIN leads AS l ON s.visitor_id =l.visitor_id AND t.last_visit <= l.created_at
-WHERE closing_reason  = 'Успешная продажа' OR status_id = 142
-ORDER BY l.amount DESC NULLS LAST, visit_date, utm_source, utm_medium, utm_campaign
---LIMIT 10;
+    SELECT
+        s.visitor_id,
+        s.visit_date,
+        s.source AS utm_source,
+        s.medium AS utm_medium,
+        s.campaign AS utm_campaign,
+        l.lead_id,
+        l.created_at,
+        l.amount,
+        l.closing_reason,
+        l.status_id,
+        l.created_at - s.visit_date AS diff
+    FROM tab AS t
+    INNER JOIN sessions AS s
+        ON t.visitor_id = s.visitor_id
+        AND t.last_visit = s.visit_date
+    LEFT JOIN leads AS l
+        ON s.visitor_id =l.visitor_id
+        AND t.last_visit <= l.created_at
+    WHERE closing_reason  = 'Успешная продажа' OR status_id = 142
+    ORDER BY
+        l.amount DESC NULLS LAST,
+        visit_date ASC,
+        utm_source ASC,
+        utm_medium ASC,
+        utm_campaign ASC
 )
 SELECT
     PERCENTILE_DISC(0.9) WITHIN GROUP (ORDER BY diff) AS p_90
 FROM diff_tab
 
+-- count organic and traffic visits
 WITH tab AS (
     SELECT
         visitor_id,
